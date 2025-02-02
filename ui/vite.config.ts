e25809de07816b2470609c11a084a2d0db9668b5
Anton Neld <antonherrneld@gmail.com>
2025-02-02 16:42:39 +0100

    Open UI automatically like before

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    open: "/?core=localhost%3A8001",
+    port: 3000
+  },
   plugins: [react()],
 })
