765f7eb6b574ff540aa9955f6985a68732129773
Anton Neld <antonherrneld@gmail.com>
2025-02-02 18:24:33 +0100

    Don't clear the screen

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  clearScreen: false,
   server: {
     open: "/?core=localhost%3A8001",
     port: 3000
